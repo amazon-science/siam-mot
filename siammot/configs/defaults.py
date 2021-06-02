95e74fd69e4e3d4f6ba7db20f5e18f5aa1fda2ff
Bing Shuai <bshuai@amazon.com>
2021-06-02 10:22:02 -0700

    Refactor configuration file

--- siammot/configs/defaults.py
+++ siammot/configs/defaults.py
@@ -2,18 +2,29 @@ from yacs.config import CfgNode as CN
 
 from maskrcnn_benchmark.config import cfg
 
-# Root directory of datasets
-cfg.DATASETS.ROOT_DIR = ''
+# default detector config (Rewrite some of them in cfg)
+cfg.MODEL.META_ARCHITECTURE = 'GeneralizedRCNN'
+cfg.MODEL.BACKBONE.CONV_BODY = 'DLA-34-FPN'
 
-# all video-related parameters
-cfg.VIDEO = CN()
-# the length of video clip for training/testing
-cfg.VIDEO.TEMPORAL_WINDOW = 8
-# the temporal sampling frequency for training
-cfg.VIDEO.TEMPORAL_SAMPLING = 4
-cfg.VIDEO.RANDOM_FRAMES_PER_CLIP = 2
+cfg.MODEL.RPN.USE_FPN = True
+cfg.MODEL.RPN.ANCHOR_STRIDE = (4, 8, 16, 32, 64)
+cfg.MODEL.RPN.ANCHOR_SIZES = (32, 64, 128, 256, 512)
+cfg.MODEL.RPN.PRE_NMS_TOP_N_TRAIN = 2000
+cfg.MODEL.RPN.PRE_NMS_TOP_N_TEST = 1000
+cfg.MODEL.RPN.POST_NMS_TOP_N_TEST = 300
+cfg.MODEL.RPN.FPN_POST_NMS_TOP_N_TEST = 300
+
+cfg.MODEL.ROI_HEADS.USE_FPN = True
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256
 
 cfg.MODEL.BOX_ON = True
+cfg.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION = 7
+cfg.MODEL.ROI_BOX_HEAD.POOLER_SCALES = (0.25, 0.125, 0.0625, 0.03125)
+cfg.MODEL.ROI_BOX_HEAD.POOLER_SAMPLING_RATIO = 2
+cfg.MODEL.ROI_BOX_HEAD.FEATURE_EXTRACTOR = "FPN2MLPFeatureExtractor"
+cfg.MODEL.ROI_BOX_HEAD.PREDICTOR = "FPNPredictor"
+cfg.MODEL.ROI_BOX_HEAD.NUM_CLASSES = 2
+cfg.MODEL.ROI_BOX_HEAD.MLP_HEAD_DIM = 1024
 
 # DLA
 cfg.MODEL.DLA = CN()
@@ -25,7 +36,7 @@ cfg.MODEL.DLA.BACKBONE_OUT_CHANNELS = 128
 cfg.MODEL.DLA.STAGE_WITH_DCN = (False, False, False, False, False, False)
 
 # TRACK branch
-cfg.MODEL.TRACK_ON = False
+cfg.MODEL.TRACK_ON = True
 cfg.MODEL.TRACK_HEAD = CN()
 cfg.MODEL.TRACK_HEAD.TRACKTOR = False
 cfg.MODEL.TRACK_HEAD.POOLER_SCALES = (0.25, 0.125, 0.0625, 0.03125)
@@ -70,6 +81,14 @@ cfg.MODEL.TRACK_HEAD.EMM.CLS_POS_REGION = 0.8
 # such as caltech roadside pedestrian
 cfg.MODEL.TRACK_HEAD.EMM.COSINE_WINDOW_WEIGHT = 0.4
 
+# all video-related parameters
+cfg.VIDEO = CN()
+# the length of video clip for training/testing
+cfg.VIDEO.TEMPORAL_WINDOW = 8
+# the temporal sampling frequency for training
+cfg.VIDEO.TEMPORAL_SAMPLING = 4
+cfg.VIDEO.RANDOM_FRAMES_PER_CLIP = 2
+
 #Inference
 cfg.INFERENCE = CN()
 cfg.INFERENCE.USE_GIVEN_DETECTIONS = False
@@ -81,7 +100,10 @@ cfg.SOLVER.CHECKPOINT_PERIOD = 5000
 cfg.SOLVER.VIDEO_CLIPS_PER_BATCH = 16
 
 #Input
-cfg.INPUT.MOTION_LIMIT = 0.05
+cfg.INPUT.MOTION_LIMIT = 0.1
 cfg.INPUT.COMPRESSION_LIMIT = 50
 cfg.INPUT.MOTION_BLUR_PROB = 0.5
 cfg.INPUT.AMODAL = False
+
+# Root directory of datasets
+cfg.DATASETS.ROOT_DIR = ''
